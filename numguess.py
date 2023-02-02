f3c09f021d1b8fe66d3c8f3467ec31b0b73696f4
keejuneman <rlwns9603@naver.com>
2023-02-02 12:44:10 +0900

    fix: Changed DataType of guess, str to int

--- numguess.py
+++ numguess.py
@@ -9,7 +9,7 @@ print(answer)
 # Get user's name, guess
 
 user_name = input("Hello, there! What is your name? ")
-guess = input(f'Hi, {user_name}. Guess the number(1-100) : ')
+guess = int(input(f'Hi, {user_name}. Guess the number(1-100) : '))
 
 # Print to check(for debugging)
 print(user_name, guess)
